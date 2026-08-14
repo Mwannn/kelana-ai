8956e6041e3da1088f7a2e5e3d393a62d9f9a6a4
Marwan Wisnu <marwanwisnu06@gmail.com>
2026-08-14 20:22:12 +0700

    Convert KelanaAI into FastAPI

--- backend/main.py
+++ backend/main.py
@@ -1,44 +1,31 @@
-from services.trip_service import (
-    get_trip_category, 
-    get_travel_session, 
-    calculate_daily_budget, 
-    get_recommended_places
-)
+from fastapi import FastAPI
+from pydantic import BaseModel
+from services.trip_service import calculate_daily_budget, get_trip_category
 
-def main():
-    print("==================================")
-    print("KelanaAI - Input")
-    print("==================================")
-    # Tangani I/O
-    destination = input("Destination (default: Japan): ") or "Japan"
+app = FastAPI()
+
+class TripRequest(BaseModel):
+    destination: str
+    days: int
+    budget: float
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to KelanaAI"}
+
+@app.get("/health")
+def health_check():
+    return {"status": "OK"}
+
+@app.post("/api/v1/trips")
+def create_trip(trip_request: TripRequest):
+    daily_budget = calculate_daily_budget(trip_request.budget, trip_request.days)
+    category = get_trip_category(trip_request.budget)
     
-    days_input = input("Days (default: 5): ")
-    days = int(days_input) if days_input.strip() else 5
-    
-    budget_input = input("Budget USD (default: 1500): ")
-    budget = float(budget_input) if budget_input.strip() else 1500.0
-    
-    month = input("Travel Month (default: December): ") or "December"
-
-    category = get_trip_category(budget)
-    daily_budget = calculate_daily_budget(budget, days)
-    season = get_travel_session(month)
-    recommended_places = get_recommended_places(destination)
-
-    print("\n==================================")
-    print("KelanaAI")
-    print("==================================")
-    print(f"Destination     : {destination.capitalize()}")
-    print(f"Days        : {days}")
-    print(f"Budget       : {int(budget) if budget.is_integer() else budget} USD")
-    print(f"Category      : {category}")
-    print(f"Daily Budget    : {int(daily_budget) if daily_budget.is_integer() else daily_budget} USD/Day")
-    print(f"Travel Month: {month.capitalize()}")
-    print(f"Season : {season}\n")
-
-    print("Recommended Places")
-    for place in recommended_places:
-        print(f"- {place}")
-
-if __name__ == "__main__":
-    main()
+    return {
+        "destination": trip_request.destination,
+        "days": trip_request.days,
+        "budget": trip_request.budget,
+        "daily_budget": daily_budget,
+        "category": category
+    }
